ae44527d70f2b90726210a16973fafee9047ebfa
rinkusharma87 <contact.rinkusharma@gmail.com>
2016-11-13 12:05:01 +0800

    applicationContext.xml (customized added)

--- inventory_management_system/backend/src/main/java/com/tap/BackendApplication.java
+++ inventory_management_system/backend/src/main/java/com/tap/BackendApplication.java
@@ -2,8 +2,10 @@ package com.tap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value = "applicationContext.xml")
 public class BackendApplication {
 
 	public static void main(String[] args) {
