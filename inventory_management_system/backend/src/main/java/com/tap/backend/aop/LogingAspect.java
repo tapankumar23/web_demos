f914e57870178aaf189cac29c971acdd56d71b33
rinkusharma87 <contact.rinkusharma@gmail.com>
2016-11-13 20:54:24 +0800

    added @Pointcut instead of @Brfore in Aspect

--- inventory_management_system/backend/src/main/java/com/tap/backend/aop/LogingAspect.java
+++ inventory_management_system/backend/src/main/java/com/tap/backend/aop/LogingAspect.java
@@ -2,11 +2,12 @@ package com.tap.backend.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LogingAspect {
 
-	@Before("execution(public String getName())")
+	@Pointcut("execution(public String getName())")
 	public void loggingAspect(){
 		System.out.println("This is default advise");
 	}
